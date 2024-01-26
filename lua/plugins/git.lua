6f2742ac52dba686f08a44455f5e5ac96bb4bb79
James Ryan <jryan@letsgetchecked.com>
2024-01-26 12:50:35 +0000

    update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,11 +14,11 @@ return {
                 changedelete = { text = '~' },
             },
             on_attach = function(bufnr)
-                vim.keymap.set('n', '<leader>gp', require('lua.plugins.gitsigns').prev_hunk,
+                vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
                     { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-                vim.keymap.set('n', '<leader>gn', require('lua.plugins.gitsigns').next_hunk,
+                vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
                     { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-                vim.keymap.set('n', '<leader>ph', require('lua.plugins.gitsigns').preview_hunk,
+                vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
                     { buffer = bufnr, desc = '[P]review [H]unk' })
             end,
         }
